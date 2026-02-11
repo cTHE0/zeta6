878f9250afa8c62abfadf46ff72ecc8f26d5ff34
cTHE0 <theo.couerbe@gmail.com>
2026-02-12 01:54:11 +0800

    simple

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import socket,json,threading,sys,os
 u=sys.argv[1];f=f"m_{u}.json";h=json.loads(open(f).read())if os.path.exists(f)else[]
-s=socket.create_connection(('65.75.201.11',12345));s.sendall((json.dumps({'u':u})+'\n').encode())
+s=socket.create_connection(('65.75.201.11',12345));s.sendall((json.dumps({'u':u})+'\n').encode());print(f"@{u} connecté")
 def r():
  b=''
  while 1:
@@ -10,5 +10,5 @@ threading.Thread(target=r,daemon=1).start()
 while 1:
  l=input(f"{u}> ").strip()
  if l=='/q':break
- t=l.split(' ',1);d=t[0]if len(t)>1else'*';m={'to':d,'t':t[1]if len(t)>1else t[0]};s.sendall((json.dumps(m)+'\n').encode());h.append({'f':u,'to':d,'t':m['t']});open(f,'w').write(json.dumps(h))
+ t=l.split(' ',1);d=t[0] if len(t)>1 else '*';m={'to':d,'t':t[1] if len(t)>1 else t[0]};s.sendall((json.dumps(m)+'\n').encode());h.append({'f':u,'to':d,'t':m['t']});open(f,'w').write(json.dumps(h))
 s.close()
--- server.py
+++ server.py
@@ -1,12 +1,12 @@
 import socket, threading, json
-c={};s=socket.socket();s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1);s.bind(('',12345));s.listen()
+print("✓ serveur actif 65.75.201.11:12345");c={};s=socket.socket();s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1);s.bind(('',12345));s.listen()
 def h(x,a):
- u=json.loads(x.recv(99).decode())['u'];c[x]=u
+ u=json.loads(x.recv(99).decode())['u'];c[x]=u;print(f"+ @{u}")
  while 1:
   try:
    m=json.loads(x.recv(999).decode());m['f']=u
-   for y in c: 
-    if y!=x: y.sendall((json.dumps(m)+'\n').encode())
-  except: break
- x.close();c.pop(x,0)
-while 1: threading.Thread(target=h,args=s.accept(),daemon=1).start()
+   for y in c:
+    if y!=x:y.sendall((json.dumps(m)+'\n').encode())
+  except:break
+ print(f"- @{c.pop(x,'?')}");x.close()
+while 1:threading.Thread(target=h,args=s.accept(),daemon=1).start()
